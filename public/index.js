38452470dc1ba68cb2ea122595344f0741c242bc
Roman <vizionssmith@gmail.com>
2022-02-15 20:44:59 +0800

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,33 +1,48 @@
-let buttonTask = document.querySelector("#buttonAdd");
-let tasks = document.querySelector(".tasks");
+let button = document.querySelector('#buttonAdd');
+let list = document.querySelector('#newTask');
+let todo;
+
+function toLocal(){
+    todo = list.innerHTML;
+    localStorage.setItem('note', todo);
+}
+
+if(localStorage.getItem("note")){
+    list.innerHTML = localStorage.getItem('note');
+}
 
 
-buttonTask.addEventListener("click", ev => {
-        let newTsk = document.createElement("div");
-        let inputTasks = document.querySelector("#inputTsk").value;
-        let txt = document.createTextNode(inputTasks);
-        newTsk.appendChild(txt);
+button.addEventListener('click', ()=> {
+    let li = document.createElement('li');
+    let inputTask = document.querySelector('#inputTasks').value;
+    let t = document.createTextNode(inputTask);
 
-        if(inputTasks == "") {
-            alert("pls add text tasks");
-        }else {
-            tasks.appendChild(newTsk);
-        }
-        document.querySelector("#inputTsk").value = "";
-        let span = document.createElement("span");
-        let x = document.createTextNode(" X");
-        span.className= "close";
-        span.appendChild(x);
-        newTsk.appendChild(span);
+    li.appendChild(t);
+    if(inputTask == ''){
+        alert('add task')
+    }else{
+        list.appendChild(li);
+    }
+
+    document.getElementById('inputTasks').value = '';
+    let span = document.createElement('span');
+    let x = document.createTextNode('X');
+    span.className = 'close';
+    span.appendChild(x);
+    li.appendChild(span);
+
+    toLocal();
 })
 
-//deleted tsk
-tasks.addEventListener("click", ev => {
-    if(ev.target.tagName == "DIV"){
-        ev.target.classList.toggle("checked");
-    }else if(ev.target.tagName == "SPAN"){
-        let div = ev.target.parentNode;
-        ev.target.classList.toggle("checked");
+//delete tsk
+
+list.addEventListener('click', event => {
+    if(event.target.tagName == 'LI'){
+        event.target.classList.toggle('checked');
+        toLocal();
+    }else if(event.target.tagName == 'SPAN'){
+        let div = event.target.parentNode;
         div.remove();
+        toLocal();
     }
 })
